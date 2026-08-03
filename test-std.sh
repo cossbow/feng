f6d4f11b53c4f455dfaae58bbf109a7f7e483e06
客串一回 <cossbow@qq.com>
2026-08-03 22:06:00 +0800

    Add single test in test-std.sh

--- test-std.sh
+++ test-std.sh
@@ -10,7 +10,7 @@ if [[ -z "$jar" ]]; then
     jar=$(findJar)
 fi
 
-cmd="java -Dfeng.asan -jar ${jar}"
+cmd="java -Dfeng.asan=true -jar ${jar}"
 args="-Lstd=std -p test -t f -b m -D -T"
 
 out="target/test-std"
@@ -21,16 +21,35 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-i=1
-ls tests | while read t; do
-    if [ "${t##*.}" != "feng" ]; then
-        continue
-    fi
-    td="${out}/tmp-${i}"
-    echo "=== testing ${t} ==="
-    mkdir -p ${td}
-    $cmd $args -i tests/${t} -o ${td}
-    ${td}/test
-    i=$((i+1))
-done
+function test_all() {
+  i=1
+  ls tests | while read t; do
+      if [ "${t##*.}" != "feng" ]; then
+          continue
+      fi
+      td="${out}/tmp-${i}"
+      echo "=== testing ${t} ==="
+      mkdir -p ${td}
+      $cmd $args -i tests/${t} -o ${td}
+      ${td}/test
+      i=$((i+1))
+  done
+}
 
+function test_one() {
+    c=$1
+    f=$c
+    if [ "${c##*.}" != "feng" ]; then
+        f="${f}.feng"
+    fi
+    td="${out}/tmp-${c}"
+    mkdir -p ${td}
+    $cmd $args -i tests/${f} -o ${td}
+    ${td}/test
+}
+
+if [[ -z "${1}" ]]; then
+    test_all
+else
+    test_one "${1}"
+fi
